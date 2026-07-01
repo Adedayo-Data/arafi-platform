ce75584c9ea0dca82feca75d437171dc4f7519eb
Adedayo-Data <theophilusdayo@outlook.com>
2026-07-01 20:43:25 +0100

    chore: Initail monorepo project scaffolding

---
+++ webhookreceiver/src/main/java/com/yourara/arafi/WebhookreceiverApplication.java
@@ -0,0 +1,13 @@
+package com.yourara.arafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookreceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookreceiverApplication.class, args);
+	}
+
+}
