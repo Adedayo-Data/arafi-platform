78dbef086961416f0d695f8c32dfd6ddc40dd3b0
Adedayo-Data <theophilusdayo@outlook.com>
2026-07-02 18:26:15 +0100

    chore(api): initialize arafi-api Spring Boot project structures and build configs

---
+++ arafi-api/src/main/java/com/yourara/arafi/ArafiApiApplication.java
@@ -0,0 +1,13 @@
+package com.yourara.arafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArafiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArafiApiApplication.class, args);
+	}
+
+}
