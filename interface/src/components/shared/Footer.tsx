4070168574917189c4e19cf504b0f66856759150
Adedayo-Data <theophilusdayo@outlook.com>
2026-07-11 05:35:25 +0100

    chore: update copyright year to 2026 in footer

--- interface/src/components/shared/Footer.tsx
+++ interface/src/components/shared/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
             </span>
           </span>
           <p className="font-body-md text-on-surface/40">
-            © 2025 Arafi. All rights reserved.
+            © 2026 Arafi. All rights reserved.
           </p>
         </div>
         <div className="flex flex-wrap justify-center gap-8">
